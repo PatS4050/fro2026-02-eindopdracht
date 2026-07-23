a556b0d4a33c02208efd52a1b8f63b6c4abe931b
PatS4050 <patrick@straatman.eu>
2026-07-23 14:21:19 +0200

    Forecast link gemaakt - huidige positie werkt niet

--- src/components/forecast/Forecast.jsx
+++ src/components/forecast/Forecast.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './Forecast.css'
 import iconForecast from "../../assets/navigation/IconForecast.svg"
 import { Link } from 'react-router-dom';
 
-function Forecast() {
+function Forecast(location) {
+    const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+    // const forecastLink = `https://api.openweathermap.org/data/4.0/onecall/timeline/1h?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${apiKey}`;
+    const forecastLink = `https://api.openweathermap.org/data/4.0/onecall/timeline/1h?lat=78.22334&lon=16.64689&units=metric&appid=${apiKey}`;
+    // const forecastLink = `https://api.openweathermap.org/data/4.0/onecall/timeline/1h?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${apiKey}`;
+
+
+    const [forecastH, setForecastH] = useState([]);
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+
+    async function fetchfForecastH() {
+        try {
+            const response = await axios.get(forecastLink, {})
+            setForecastH(response.data);
+            console.log(response.data);
+
+        } catch (e) {
+            toggleError(true)
+            console.error(e);
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        void fetchfForecastH();
+    }, [location]);
+
     return (
         <div className="box-m">
             <span className="row-start">
