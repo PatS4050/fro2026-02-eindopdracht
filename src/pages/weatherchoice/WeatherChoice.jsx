c1a7038568c00ef5fd7e13179282b01bd1aaaee4
PatS4050 <patrick@straatman.eu>
2026-07-29 20:37:05 +0200

    Weerkeuze opzet

--- src/pages/weatherchoice/WeatherChoice.jsx
+++ src/pages/weatherchoice/WeatherChoice.jsx
@@ -6,7 +6,12 @@ function WheatherChoice() {
         <>
             <main>
                 <h2>Weer keuze</h2>
-
+                <p>Open-Meteo als API</p>
+                <p>Haal grid op met radius ca 250mijl rond huidige locatie</p>
+                <p>kies windrichting</p>
+                <p>kies windkracht in m/2</p>
+                <button>zoek</button>
+                <p>lijst met plaatsen met windrichting en windkracht</p>
             </main>
 
         </>
