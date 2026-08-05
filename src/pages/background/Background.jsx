7ea6d72b9e6d84cd0a493e455ab81b2e8d3f4db7
PatS4050 <patrick@straatman.eu>
2026-08-05 22:18:36 +0200

    navbar aangemaakt en de defaultplaatjes aangemaakt

--- src/pages/background/Background.jsx
+++ src/pages/background/Background.jsx
@@ -1,14 +1,18 @@
 import React, {useState, useEffect} from 'react';
 import './Background.css'
-import { Link } from 'react-router-dom';
+import bg1 from "../../assets/background/background-1.jpg"
+import bg2 from "../../assets/background/background-2.jpg"
+import bg3 from "../../assets/background/background-3.jpg"
+import NavBarPage from "../../components/navbar/NavBarPage.jsx";
 
-function Background() {
+
+function Background({location}) {
 
     const [backgroundImage, setBackgroundImage] = useState(
         localStorage.getItem("backgroundImage") || ""
     );
     const [preview, setPreview] = useState("");
-
+    const defaultBackgrounds = [bg1,bg2,bg3]
     const changeBackground = (image) => {
         setBackgroundImage(image);
         localStorage.setItem("backgroundImage", image);
@@ -42,6 +46,7 @@ function Background() {
 
     return (
         <>
+            <NavBarPage location={location} />
             <main className="content">
                 <h2>Achtergrond wijzigen</h2>
                 <p>Upload hier een image</p>
@@ -50,8 +55,22 @@ function Background() {
                     accept="image/*"
                     onChange={handleUpload}
                 />
-                <p>6 voorbeeld plaatjes</p>
-                {preview && (
+                <div className="gallery">
+                    {defaultBackgrounds.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Achtergrond ${index + 1}`}
+                            onClick={() => changeBackground(image)}
+                            style={{
+                                width: "70px",
+                                cursor: "pointer",
+                                margin: "6px",
+                                borderRadius: "6px",
+                            }}
+                        />
+                    ))}
+                </div>                {preview && (
                     <img
                         src={preview}
                         alt="Preview"
