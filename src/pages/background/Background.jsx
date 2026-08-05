fa60bb989313dfe1fa72794e5780adbf3b0380b3
PatS4050 <patrick@straatman.eu>
2026-08-05 13:56:09 +0200

    Upload veld gemaakt

--- src/pages/background/Background.jsx
+++ src/pages/background/Background.jsx
@@ -3,11 +3,22 @@ import './Background.css'
 import { Link } from 'react-router-dom';
 
 function Background() {
+
+    const handleUpload = (e) => {
+        const file = e.target.files[0];
+        onUpload(file);
+    };
+
     return (
         <>
-            <main>
+            <main className="content">
                 <h2>Achtergrond wijzigen</h2>
-                <p>Upload een image</p>
+                <p>Upload hier een image</p>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleUpload}
+                />
                 <p>6 voorbeeld plaatjes</p>
                 <button>Apply als background</button>
             </main>
