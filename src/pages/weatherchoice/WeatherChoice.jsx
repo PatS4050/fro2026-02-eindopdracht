b56451239ee3041e517e543272ba1e9833e873a9
PatS4050 <patrick@straatman.eu>
2026-08-06 09:18:09 +0200

    windroos op pagina gezet

--- src/pages/weatherchoice/WeatherChoice.jsx
+++ src/pages/weatherchoice/WeatherChoice.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import windRose from "../../assets/navigation/windroos/windRoosSimple.svg";
 
 function WheatherChoice() {
     return (
@@ -10,6 +11,20 @@ function WheatherChoice() {
                 <p>Haal grid op met radius ca 250mijl rond huidige locatie</p>
                 <p>kies windrichting</p>
                 <p>kies windkracht in m/2</p>
+                <div className="basis">
+                    <img
+                        src={windRose}
+                        alt='windroos'
+                        style={{
+                            transform: `rotate( 15deg)`,
+                            transition: "transform 0.6s ease"
+                        }}
+                    />
+                    <ul className="basis">
+                        <li className="row-center"><h4>FORCE</h4></li>
+                    </ul>
+                </div>
+
                 <button>zoek</button>
                 <p>lijst met plaatsen met windrichting en windkracht</p>
             </main>
